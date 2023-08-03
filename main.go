475f8d3996661b2f5143f07b16addb04a88935ea
Paolo Calao <paolo.calao@gmail.com>
2023-08-03 22:37:40 +0200

    Draft todo struct

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	if err := run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+type Item struct {
+	Name    string `json:"name"`
+	Message string `json:"message"`
+}
+
+type Todo struct {
+	Items []Item `json:"items"`
+}
+
+func run() error {
+	path := "todo.json"
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return fmt.Errorf("opening file[%s]: %w", path, err)
+	}
+
+	var todo Todo
+	if err := json.Unmarshal(content, &todo); err != nil {
+		return fmt.Errorf("unmarshaling content[%s]: %w", content, err)
+	}
+
+	fmt.Println("Content: ")
+	for _, i := range todo.Items {
+		fmt.Println("Item:", i.Name, "-", "Message:", i.Message)
+	}
+
+	return nil
 }
